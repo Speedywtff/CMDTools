61db198fdcb0f1b35fe62d457b25446aff821dae
ArjavTripathi <arjavtri@iastate.edu>
2025-10-20 14:58:29 -0500

    works for substring

--- grep.c
+++ grep.c
@@ -7,18 +7,25 @@
 void readFile(char* searchable, FILE *f){
     char line[256];
     int i = 1;
+    bool match = false;
     line[strcspn(line, "\n")] = '\0';
-    while(fgets(line, sizeof(line), f) != NULL){  //DOES NOT WORK FOR SUBSTRINGS. 
-        if(strcmp(line, searchable) == 0){
-            printf("Found %s in line: %d\n", searchable, i);
+    while(fgets(line, sizeof(line), f) != NULL){ 
+        if(strstr(line, searchable) != NULL){
+            printf("Found \"%s\" in line: %d\n", searchable, i);
+            if(!match){
+                match = true;
+            }
         }
         i++;
+    }
+    if(!match){
+        printf("\"%s\" not found in file", searchable);
     }
 
 }
 
 int main(int argc, char *argv[]){
-    if(argc != 5) perror("Usage: ./search \"filename\" <PATH TO FILE> -s \"<SEARCH STRING>\"");
+    if(argc != 5) perror("Usage: ./search -f \"<PATH TO FILE>\" -s \"<SEARCH STRING>\"");
     char* search = argv[4];
     char* filename = argv[2];
 
